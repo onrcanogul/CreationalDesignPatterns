7c37dcb30cc1b96c3bd9a697c7f6bc31b5c7a5b3
onrcanogul <onurcanogul@ogr.eskisehir.edu.tr>
2024-05-03 14:28:38 +0300

    Add project files.

---
+++ Factory_Method/Program.cs
@@ -0,0 +1,144 @@
+﻿#region Bad Scenerio
+//GarantiBank garantiBank = new("asd", "123");
+//garantiBank.ConnectGaranti(); //we interrupt the coding process and focus on creating an instance 
+////..
+////..
+
+//VakıfBank vakifBank = new(new() { Email = "asd@gmail.com", UserCode = "asd" }, "123");
+//vakifBank.ValidateCredential();
+//if(vakifBank.isAuthentication) // again we interrupt the coding process
+//{
+//    //..
+//    //..
+//}
+
+//HalkBank halkBank = new("asd");
+//halkBank.Password = "123";
+#endregion
+
+
+GarantiBank? garantiBank = BankCreator.Create(BankTypes.GarantiBank) as GarantiBank;
+VakifBank? vakifBank = BankCreator.Create(BankTypes.VakifBank) as VakifBank;
+HalkBank? halkBank = BankCreator.Create(BankTypes.HalkBank) as HalkBank;
+
+#region Abstract Product
+interface IBank
+{
+
+}
+
+#endregion
+
+#region Concrete Products
+class GarantiBank : IBank
+{
+    string _userCode, _password;
+    public GarantiBank(string userCode, string password)
+    {
+        Console.WriteLine("GarantiBank");
+        _userCode = userCode;
+        _password = password;
+    }
+    public void ConnectGaranti() => Console.WriteLine($"{nameof(GarantiBank)} - Connected");
+    public void SendMoney(int amount) => Console.WriteLine($"{amount} money sent");
+}
+class HalkBank : IBank
+{
+    string _userCode, _password;
+    public HalkBank(string userCode)
+    {
+        Console.WriteLine("HalkBank");
+        _userCode = userCode;
+    }
+    public string Password { set => _password = value; }
+    public void Send(int amount, string accountNumber) => Console.WriteLine($"{amount} money sent.");
+}
+class CredentialVakifBank
+{
+    public string UserCode { get; set; }
+    public string Email { get; set; }
+}
+class VakifBank : IBank
+{
+    string _userCode, _password, _email;
+    public bool isAuthentication { get; set; }
+    public VakifBank(CredentialVakifBank credential, string password)
+    {
+        Console.WriteLine("VakifBank");
+        _userCode = credential.UserCode;
+        _email = credential.Email;
+        _password = password;
+    }
+    public void ValidateCredential()
+    {
+        if (true)
+            isAuthentication = true;
+
+        isAuthentication = false;
+    }
+
+    public void SendMoneyToAccountNumber(int amount, string recpientName, string accountNumber) => Console.WriteLine($"{amount} money sent");
+
+}
+#endregion
+
+#region Abstract Factory
+interface IBankFactory
+{
+    IBank CreateInstance();
+}
+#endregion
+
+#region Concrete Factory
+class GarantiBankFactory : IBankFactory
+{
+    public IBank CreateInstance()
+    {
+        GarantiBank garantiBank = new("asd", "123");
+        garantiBank.ConnectGaranti();
+        return garantiBank;
+    }
+}
+
+class VakifBankFactory : IBankFactory
+{
+    public IBank CreateInstance()
+    {
+        VakifBank vakifBank = new(new() { Email = "asd@gmail.com", UserCode = "asd" }, "123");
+        vakifBank.ValidateCredential();
+        return vakifBank;
+    }
+}
+
+class HalkBankFactory : IBankFactory
+{
+    public IBank CreateInstance()
+    {
+        HalkBank halkBank = new("asd123");
+        halkBank.Password = "123";
+        return halkBank;
+    }
+}
+
+#endregion
+
+#region Creator
+enum BankTypes
+{
+    VakifBank, HalkBank, GarantiBank
+}
+class BankCreator
+{
+    static public IBank Create(BankTypes bankType)
+    {
+        IBankFactory bankFactory = bankType switch
+        {
+            BankTypes.VakifBank => new VakifBankFactory(),
+            BankTypes.HalkBank => new HalkBankFactory(),
+            BankTypes.GarantiBank => new GarantiBankFactory()
+        };
+
+        return bankFactory.CreateInstance();
+    }
+}
+#endregion
---
+++ Singleton/Program.cs
@@ -0,0 +1,53 @@
+﻿//Get instance from UserManager
+
+UserManager userManager = UserManager.GetInstance;
+UserManager _userManager = UserManager.GetInstance; // only 1 message on console
+
+ProductManager productManager = ProductManager.GetInstance;
+ProductManager _productManager = ProductManager.GetInstance; // only 1 message on console
+
+
+//Single Responsibilty??????
+#region First way
+class UserManager
+{
+    private UserManager()
+    {
+        Console.WriteLine($"{nameof(UserManager)} has created.");    
+    }
+    static UserManager _userManager;
+    public static UserManager GetInstance // can be method aswell
+    {
+        get { 
+            if(_userManager == null)
+                _userManager = new UserManager();
+
+                return _userManager;
+            }
+    }
+        
+    
+}
+#endregion
+#region Second Way
+class ProductManager
+{
+    private ProductManager()
+    {
+        Console.WriteLine($"{nameof(ProductManager)} has created.");
+    }
+    static ProductManager()
+    {
+        _productManager = new ProductManager();
+    }
+    private static ProductManager _productManager;
+    public static ProductManager GetInstance
+    {
+        get
+        {
+            return _productManager;
+        }
+    }
+}
+
+#endregion
